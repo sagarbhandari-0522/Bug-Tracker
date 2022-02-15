e67aa6b928b8e020bd6dd06ebf31fc100325a702
sagarbhandari <sagarvandari1@gmail.com>
2022-02-15 19:26:04 +0545

    build: add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user, only: %i[edit update destroy]
+
+  def index
+    @users = User.all
+    authorize @users
+  end
+
+  def edit
+    authorize @user
+  end
+
+  def update
+    if @user.update(user_params)
+      authorize @user
+      redirect_to users_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    authorize @user
+
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit({ role_ids: [] })
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+end
