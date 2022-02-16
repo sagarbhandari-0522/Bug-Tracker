31dd5109fc4dab9c8a8a87c28c91adaa83b58771
sagarbhandari <sagarvandari1@gmail.com>
2022-02-16 12:36:50 +0545

    build: layouts page on sign in

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class DashboardsController < ApplicationController
+  def index
+    # redirect_to dashboard_path(current_user) if user_signed_in?
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_one_attached :profile_picture
   rolify
