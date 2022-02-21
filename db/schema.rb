c73b15f2f12be5c5fe176dfa32f59bb8e2373d70
sagarbhandari <sagarvandari1@gmail.com>
2022-02-21 12:05:10 +0545

    Adding bugs model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_21_055206) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_054952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,13 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_21_055206) do
   create_table "bugs", force: :cascade do |t|
     t.string "title"
     t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commentor"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
