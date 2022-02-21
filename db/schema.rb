f8e7784830488d92a0e6c85fb3ca81b7dc18f062
sagarbhandari <sagarvandari1@gmail.com>
2022-02-21 12:03:53 +0545

    Adding comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_221_052_752) do
+ActiveRecord::Schema[7.0].define(version: 20_220_221_055_206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -46,6 +46,13 @@ ActiveRecord::Schema[7.0].define(version: 20_220_221_052_752) do
   create_table 'bugs', force: :cascade do |t|
     t.string 'title'
     t.string 'status'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commentor'
+    t.text 'body'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
