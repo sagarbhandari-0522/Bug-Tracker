919c8e8d81f59dd744b160358a99a723493505cb
sagarbhandari <sagarvandari1@gmail.com>
2022-02-22 20:12:34 +0545

    feat:bugs crud operation

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@
 Rails.application.routes.draw do
   # resources :dashboards
   devise_for :users
-  resources :projects
+  resources :projects do
+    resources :bugs
+  end
+  post '/assign_ticket_developer', to: 'projects#assign_ticket', as: 'assign_ticket_developer'
   get '/project_manager/:id', to: 'dashboards#project_manager', as: 'project_manager'
   get '/developer/:id', to: 'dashboards#developer', as: 'developer'
   get '/tester/:id', to: 'dashboards#tester', as: 'tester'
   get '/dashboards', to: 'dashboards#index', as: 'dashboards'
+  get '/bug/:id', to: 'projects#assign', as: 'assign'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root 'welcome_page#index'
 end
